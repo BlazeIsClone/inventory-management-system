90e6ec7e046c7d14d7f6c9805b2101325dbc04f5
BlazeIsClone <64116737+BlazeIsClone@users.noreply.github.com>
2023-01-14 09:12:08 +0530

    update: minor config sidebar update

--- config/filament.php
+++ config/filament.php
@@ -246,7 +246,7 @@ return [
         'sidebar' => [
             'is_collapsible_on_desktop' => true,
             'groups' => [
-                'are_collapsible' => true,
+                'are_collapsible' => false,
             ],
             'width' => null,
             'collapsed_width' => null,
